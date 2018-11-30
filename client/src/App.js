333cb1cc67a8dd4c849b8325deaa6d178af9cb53
Jack Morrissey <j.morrissey.16@gmail.com>
2018-11-30 01:42:37 -0600

    more authO work

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import {Navbar, NavItem, Icon} from 'react-materialize'
+import { Navbar, NavItem, Icon } from 'react-materialize'
+import { Link } from 'react-router-dom';
 import './App.css'
 
 
@@ -28,10 +29,18 @@ class App extends Component {
       <div class="nav-wrapper brown darken-3">
         <div class="container">
           <Navbar brand='drink order' class="navbar brown darken-3" right>
-            <NavItem><Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>Login</Link></NavItem>
-            <NavItem><Link to="/Home" className={window.location.pathname === "/Home" ? "nav-link active" : "nav-link"}>Home</Link></NavItem>
-            <NavItem><Link to="/LiquorList" className={window.location.pathname === "/LiquorList" ? "nav-link active" : "nav-link"}>Liquor</Link></NavItem>
-            <NavItem><Link to="/DrinkList" className={window.location.pathname === "/DrinkList" ? "nav-link active" : "nav-link"}>Drink List</Link></NavItem>
+            <NavItem>
+              <Link to="/" 
+              className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>Login</Link></NavItem>
+            <NavItem>
+              <Link to="/Home" 
+              className={window.location.pathname === "/Home" ? "nav-link active" : "nav-link"}>Home</Link></NavItem>
+            <NavItem>
+              <Link to="/LiquorList" 
+              className={window.location.pathname === "/LiquorList" ? "nav-link active" : "nav-link"}>Liquor</Link></NavItem>
+            <NavItem>
+              <Link to="/DrinkList" 
+              className={window.location.pathname === "/DrinkList" ? "nav-link active" : "nav-link"}>Drink List</Link></NavItem>
           </Navbar>
         </div>
       </div>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Router } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import DrinkList from "./pages/DrinkList/DrinkList"
-import Home from "./pages/HomeLoggedIn/HomeLoggedIn";
-import LiquorList from "./pages/LiquorList/LiquorList";
 import App from "./App";
-
+import Login from './pages/Login/Login';
+import Callback from "./Callback/Callback"
+import Home from './pages/HomeLoggedIn/HomeLoggedIn'
+import LiquorList from "./pages/LiquorList/LiquorList"
+import DrinkList from "./pages/DrinkList/DrinkList"
 import Auth from "./Auth/Auth";
+import history from './history'
 
 const auth = new Auth();
 
@@ -17,13 +18,13 @@ const auth = new Auth();
 
 
 return (
-    <Router>
-        <div>
-          <NavTabs />
-          <Route exact path="/" component={Login} />
-          <Route exact path="/Home" component={Home} />
-          <Route exact path="/LiquorList" component={LiquorList} />
-          <Route exact path="/DrinkList" component={DrinkList} />
-        </div>
-      </Router>
-  )
+      <Router>
+          <div>
+            <NavTabs />
+            <Route exact path="/" component={Login} />
+            <Route exact path="/Home" component={Home} />
+            <Route exact path="/LiquorList" component={LiquorList} />
+            <Route exact path="/DrinkList" component={DrinkList} />
+          </div>
+        </Router>
+    )
